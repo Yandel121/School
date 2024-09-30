653909584e49cf330e93c15740528518f1c85516
Yandel1757 <yandelcolon76@gmail.com>
2024-09-30 13:18:02 -0400

    updated exercise

---
+++ Laboratorio1 Modulo2/src/Age.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Age {
+    public static void main(String[] args) {
+        /*4. Write, compile, and test a class to represent the logic of a program that
+                allows the user to enter values for the current year and the user’s birth year. The
+        program outputs the age of the user this year. Save the class as Age.java.*/
+
+        //Declaration//
+        String PROMPT = "Introduce el año actual";
+        String PROMPT2 ="Introduzca el año de nacimiento";
+
+        Scanner sc = new Scanner(System.in);
+        Sys
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
