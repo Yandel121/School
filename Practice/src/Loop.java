d9d605927d036788e7f7824bb7f3e2508e30faf3
Yandel1757 <yandelcolon76@gmail.com>
2024-10-30 14:15:59 -0400

    Cunty2.0

---
+++ Practice/src/Loop.java
@@ -0,0 +1,35 @@
+public class Loop {
+    public static void main(String[] args) {
+
+        //while loop//
+        int x=1;
+                while(x<=1000){
+                    System.out.println(x);
+                    x=x+1;
+                }
+
+                //for loop//
+                for (int i=1;i<=200;i=i+1){
+                    System.out.println(i);
+                }
+
+
+                int countdown = 3;
+        while (countdown > 0) {
+            System.out.println(countdown);
+            countdown--;
+        }
+
+        System.out.println("Happy New Year!!");
+//Do while loop//
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        }while(i<=100);
+
+
+    }
+}
+
+
