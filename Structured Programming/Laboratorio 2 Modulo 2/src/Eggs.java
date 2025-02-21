884e76626e541646cf364cd651fb7db936e9ac88
yande <yandelcolon76@gmail.com>
2025-02-20 20:07:27 -0400

    Hallo

---
+++ Structured Programming/Laboratorio 2 Modulo 2/src/Eggs.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Eggs {
+    /*Meadowdale Dairy Farm sells organic brown eggs to local customers. It charges $8.50 for a dozen eggs, or 71 cents
+    for individual eggs that are not part of a dozen. Write a program that prompts a user for the number of eggs in the
+    order and then display the amount owed with a full explanation. For example, typical output might be,
+    you ordered 27 eggs. That’s 2 dozen at $8.50 per dozen and 3 loose eggs at 71 cents each for a total of $19.13
+    Save the program as Eggs.java.*/
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       /* System.out.println("Welcome to Meadowdale Dairy Farm. How may we help you?");
+        System.out.println("You bought some eggs? How many?");
+        double singleEggPrice = 0.71;
+        int eggs = sc.nextInt();
+        System.out.println("You have " + eggs + " eggs. Correct?");
+        System.out.println("You have " + singleEggPrice*eggs);*/
+
+    }
+    public static double service_Selection(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many eggs? (1: Price for a dozen 2. Price per egg) 3. Price for both");
+        double service= sc.nextInt();
+        switch (service) {
+            case 1:
+                double serviceDozen = 8.50;
+                System.out.println(serviceDozen);
+                System.out.println("How many packages?");
+                double packages = sc.nextInt();
+                System.out.println("Correct?"+packages);
+                double total = serviceDozen + packages;
+                System.out.println(total);
+                break;
+                case 2:
+                    double singleEgg =0.71;
+                    System.out.println(singleEgg);
+                    System.out.println("How many eggs?");
+                    double eggs = sc.nextInt();
+                    System.out.println("Price for eggs:"+eggs*singleEgg);
+                    break;
+                    case 3:
+                        double singleDozen = 8.50;
+                        System.out.println(singleDozen);
+                        System.out.println("How many packages?");
+
+
+
+        }
+    }
+}
