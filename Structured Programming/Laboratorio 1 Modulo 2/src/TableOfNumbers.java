316bb7805a09d17a3cb4efc7894bf1bd8015f69b
yande <yandelcolon76@gmail.com>
2025-02-20 20:55:33 -0400

    Hallo

--- Structured Programming/Laboratorio 1 Modulo 2/src/TableOfNumbers.java
+++ Structured Programming/Laboratorio 1 Modulo 2/src/TableOfNumbers.java
@@ -1,18 +1,21 @@
 public class TableOfNumbers {
+    // File: TableOfNumbers.java
+    // Author: Yandel Colón Rivera & Ariam Rivas Sánchez
+    // Date: 2025-2-20
+    // Class: COMP 2315 Structured Programming
+    // Description: A program that shows a table from 1 to ten and showcasing that numbers power by two and square root.
     public static void main(String[] args) {
-
-       tableau();
+        tableau();
     }
-            public static void tableau() {
+
+    public static void tableau() {
         System.out.println("Table of Numbers");
-        String NUMBER="Number:";
-        String POWER="Power:";
-        String SQUARE="Square:";
-        System.out.printf("%-20s %-10s %10s",NUMBER,POWER,SQUARE);
-                for (int i = 0; i <= 10; i++) {
-                    System.out.printf("%-20d %-10d %10d %n", (i), ((int)Math.pow(i, 2)), (int)(Math.sqrt(i)));
-                }
-
-            }
+        String NUMBER = "Number:";
+        String POWER = "Power:";
+        String SQUARE = "Square:";
+        System.out.printf("%-20s %-10s %10s%n", NUMBER, POWER, SQUARE);
+        for (int i = 0; i <= 10; i++) {
+            System.out.printf("%-20d %-10d %10.2f %n", i, (int) Math.pow(i, 2), Math.sqrt(i));
         }
-
+    }
+}
