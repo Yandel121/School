d19c4969799a5f5ee0e7a08f4f43eb6495a6dea6
Yandel121 <yandelcolon76@gmail.com>
2025-03-06 09:26:35 -0400

    Add files via upload

--- potencia/src/potenciaCalculadora.java
+++ potencia/src/potenciaCalculadora.java
@@ -1,16 +1,26 @@
+import javax.swing.*;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class potenciaCalculadora {
     public static void main(String[] args) {
-      double base,exponente,resultado;
+      double base, exponente, resultado;
+      String response;
       Scanner entrada = new Scanner(System.in);
-      System.out.println("Entra el numero: ");
-      base = entrada.nextDouble();
-      System.out.println("Entra el exponente: ");
-      exponente = entrada.nextDouble();
-      resultado = Math.pow(base,exponente);
-      System.out.println("Resultado: "+resultado);
-        }
+      do{
+        System.out.println("Entra el numero: ");
+        base = entrada.nextDouble();
+        System.out.println("Entra el exponente: ");
+        exponente = entrada.nextDouble();
+        resultado = Math.pow(base, exponente);
+        System.out.println("Resultado: " + resultado);
+          JOptionPane.showInputDialog(null, resultado );
+//El programa analiza la respuesta, si este esta correcto o no
+        //Decision para repetir el programa
+        response = JOptionPane.showInputDialog("Deseas repetir el programa?(Si o No)");
+      }while(response.equalsIgnoreCase("Si"));
+      JOptionPane.showMessageDialog(null,"Adios");
+      }
     }
+
