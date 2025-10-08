c96c8d0dc5c355c60f1d4c839dea90ba6be86c26
Yandel121 <yandelcolon76@gmail.com>
2025-10-08 16:18:15 -0400

    Add files via upload

---
+++ ObjectOrientedProgramming/Inheritance Practice Excercises/src/Player.java
@@ -0,0 +1,22 @@
+public class Player {
+    protected int playerNumber;
+    protected String name;
+
+    public Player() {
+        this.playerNumber = playerNumber;
+        this.name = name;
+    }
+    public int getPlayerNumber() {
+        return playerNumber;
+    }
+    public void setPlayerNumber(int playerNumber) {
+        this.playerNumber = playerNumber;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
