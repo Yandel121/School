ee03c8d73e40cd2599521f21009c84ece3810878
Yandel121 <yandelcolon76@gmail.com>
2026-03-04 22:42:04 -0400

    Add files via upload

--- Data Structure/SortingAlgorithm/src/Algorithm.java
+++ Data Structure/SortingAlgorithm/src/Algorithm.java
@@ -1,150 +1,208 @@
 import java.util.Arrays;
 import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//File: Algorithm.java
+//Author: Yandel Colón Rivera
+//Date: 3/4/2026
+//Class: COMP 2900 Data Structure
+//Description: Application that demonstrates the sorting algorithms.
 public class Algorithm {
+
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 6, 7, 5, 3, 56, 23, 5, 3, 56, 36, 4, 7, 676, 6435, 845, 65, 5, 7};
-        int n = arr.length;
         Scanner sc = new Scanner(System.in);
-        int choice=0;
-        System.out.println("1. Merge Sort\n2. Bubble Sort\n3. Quick Sort\n4. Heap Sort\n5. Selection Sort");
-        System.out.println("Enter your choice: ");
-        choice= sc.nextInt();
+
+        System.out.print("Ingrese la cantidad de elementos: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Ingrese los elementos:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("\n--- MENÚ DE ORDENAMIENTO ---");
+        System.out.println("1. Bubble Sort");
+        System.out.println("2. Insertion Sort");
+        System.out.println("3. Selection Sort");
+        System.out.println("4. Quick Sort");
+        System.out.println("5. Merge Sort");
+        System.out.println("6. Heap Sort");
+        System.out.print("Seleccione una opción: ");
+
+        int choice = sc.nextInt();
+
         switch (choice) {
             case 1:
-                mergesort(arr);
+                bubbleSort(arr);
                 break;
             case 2:
-                bubbleSort(arr, n);
+                insertionSort(arr);
                 break;
-                case 3:
-                QuickSort quick = new QuickSort();
-                quick.sort(arr, 0, n - 1);
-                for (int i = 0; i < n; i++) {
-                    System.out.print(arr[i] + " ");
-                }
+            case 3:
+                selectionSort(arr);
                 break;
-                case 4:
-                HeapSort heap = new HeapSort();
-                heap.sort(arr);
-                heap.printArray(arr, n);
+            case 4:
+                quickSort(arr, 0, arr.length - 1);
                 break;
-                case 5:
-                SelectionSort selection = new  SelectionSort();
-                selection.sort(arr, n);
-                System.out.println(Arrays.toString(arr));
+            case 5:
+                mergeSort(arr, 0, arr.length - 1);
                 break;
+            case 6:
+                heapSort(arr);
+                break;
+            default:
+                System.out.println("Opción inválida.");
+                return;
         }
 
+        System.out.println("\nArreglo ordenado: " + Arrays.toString(arr));
     }
 
-    public static void mergesort(int arr[]) {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
 
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
-    }
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
 
-    public static void bubbleSort(int arr[], int n) {
-
-        for (int i = 0; i > n - 1; i++) {
-            for (int j = 1; j < n; j++) {
-                if (i > n - 1 && j > n && n > 1) {
-                    n = n - 1;
-                }
-            }
-        }
-        System.out.println(Arrays.toString(arr));
-    }
-
-    static class QuickSort {
-        int partition(int arr[], int low, int high) {
-
-            int pivot = arr[high];
-            int i = low - 1;
-            for (int j = low; j < high; j++) {
-                if (arr[j] < pivot) {
-                    i++;
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
                 }
             }
-            int temp = arr[i + 1];
-            arr[i + 1] = arr[high];
-            arr[high] = temp;
-            return i + 1;
-        }
 
-        void sort(int arr[], int low, int high) {
-            if (low < high) {
-                int pi = partition(arr, low, high);
-
-                sort(arr, low, pi - 1);
-                sort(arr, pi + 1, high);
-            }
+            if (!swapped) break;
         }
     }
 
-    static class HeapSort {
-        public void sort(int arr[]) {
-            int n = arr.length;
-            for (int i = n / 2 - 1; i >= 0; i--) {
-                heapify(arr, n, i);
-            }
-            for (int i = n - 1; i >= 0; i--) {
-                int temp = arr[0];
-                arr[0] = arr[i];
-                arr[i] = temp;
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
 
-                heapify(arr, i, 0);
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
             }
 
-        }
-
-        void heapify(int arr[], int n, int i) {
-            int largest = i;
-            int l = 2 * i + 1;
-            int r = 2 * i + 2;
-
-            if (l < n && arr[l] > arr[largest]) {
-                largest = l;
-            }
-            if (r < n && arr[r] > arr[largest]) {
-                largest = r;
-            }
-            if (largest != i) {
-                int swap = arr[i];
-                arr[i] = arr[largest];
-                arr[largest] = swap;
-                heapify(arr, n, largest);
-                System.out.println(Arrays.toString(arr));
-            }
-
-        }
-
-        void printArray(int arr[], int n) {
-            for (int i = 0; i < n; i++) {
-                System.out.print(arr[i] + " ");
-
-            }
+            arr[j + 1] = key;
         }
     }
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
 
-    static class SelectionSort {
-        void sort(int arr[], int n) {
-            for (int i = 0; i < n - 1; i++) {
-                int min = i;
-                for (int j = i + 1; j < n; j++) {
-                    if (arr[j] < arr[min]) {
-                        min = j;
-                    }
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
                 }
-                int temp = arr[min];
-                arr[min] = arr[i];
-                arr[i] = temp;
             }
+
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        System.arraycopy(arr, left, L, 0, n1);
+        System.arraycopy(arr, mid + 1, R, 0, n2);
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k++] = L[i++];
+            } else {
+                arr[k++] = R[j++];
+            }
+        }
+
+        while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
+    }
+
+    public static void heapSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            heapify(arr, n, i);
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr, i, 0);
+        }
+    }
+
+    public static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest]) largest = left;
+        if (right < n && arr[right] > arr[largest]) largest = right;
+
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            heapify(arr, n, largest);
         }
     }
 }
